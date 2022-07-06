74caabe19feced2ac1efc9b2142ab19ba26de9bf
2422112053 <2422112053@qq.com>
2022-07-06 16:14:05 +0800

    添加项目文件。

---
+++ test_7_6/test.c
@@ -0,0 +1,47 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main()
+{
+	//int age = 16;
+	// 
+	//if (age >= 18)
+	//	printf("成年\n");
+	// 
+	//else
+	//	printf("未成年\n");
+	// 
+	//return 0;
+
+
+
+
+	/*int age = 25;
+
+	if (age < 18)
+		printf("少年\n");
+
+	else if (age >= 18 && age < 26)
+		printf("青年\n");
+
+	else if (age >= 26 && age < 40)
+		printf("壮年\n");
+
+	else if (age >= 40 && age < 60)
+		printf("中年\n");
+	else
+		printf("老年\n");
+
+		return 0;*/
+
+
+
+
+
+	//int num = 7;
+
+	//if (7 == num)//两个整型变量比较时，常量放左边是一个好的代码风格
+	//	printf("haha\n");
+
+	//return 0;
+
+}
